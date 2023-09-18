bcf5df474785145e00adf7d2ea12c4450886a5fa
DArlegui <arleguidaniel@gmail.com>
2023-09-17 23:10:10 -0700

    Kind fixed drag/hover changing color effect

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const draggable_list = document.getElementById("draggable-list"); //ul
 const check = document.getElementById("check"); //Check button
 
+//Source: https://www.youtube.com/@DataIsBeautifulOfficial/videos
 const topics = [
   {
     list: ["Phython", "JavaScript", "Java", "C#", "TypeScript"],
@@ -41,26 +42,33 @@ function fixedBackground() {
 
 //Insert List Items into DOM
 function createList() {
+  //Capturing the header
   const header = document.getElementById("title");
-  const bodyBackground = document.getElementById("body");
 
+  //Changing the header and body background depending on the quiz
   header.innerHTML = topics[chosenQuiz].header;
   document.body.style.backgroundImage = topics[chosenQuiz].img;
-  fixedBackground();
+  fixedBackground(); //Prevent picture duplication
 
+  /*Clears any previous content within the HTML element with the ID "draggable-list." 
+  This is done to prepare for the creation of a new list. */
   draggable_list.innerHTML = "";
-  listItems = [];
-  console.log(topics[chosenQuiz], topics, chosenQuiz);
+  listItems = []; //stores the new list items
+
+  // console.log(topics[chosenQuiz], topics, chosenQuiz); //Check
 
   [...topics[chosenQuiz].list]
+    //Create a new array object with a value and a random number. The random number is used to shuffle the array.
     .map((a) => ({ value: a, sort: Math.random() }))
+    //Randomize the order of the array
     .sort((a, b) => a.sort - b.sort)
+    //used to extract the value from the array
     .map((a) => a.value)
     .forEach((item, index) => {
-      const listItem = document.createElement("li");
+      //iterates through the shuffled items and creates a list item (<li>) for each item.
+      const listItem = document.createElement("li"); //Creating li
 
-      // listItem.classList.add("over"); //check
-
+      //store original index. track the order of the list items. Use for drag and drop functionality
       listItem.setAttribute("data-index", index);
 
       listItem.innerHTML = `
@@ -72,46 +80,66 @@ function createList() {
         </div>
       </div>
       `;
-      listItems.push(listItem);
-      draggable_list.appendChild(listItem);
+      listItems.push(listItem); //Pushing the li item to the listItems array
+      draggable_list.appendChild(listItem); //Appending the li item to the ul
     });
 
   addEventListeners();
 }
 
+//Capture the starting index
 function dragStart() {
-  // console.log('Event: ', 'dragstart'); //check
-  dragStartIndex = +this.closest("li").getAttribute("data-index");
+  console.log("Event: ", "dragstart"); //check
+  dragStartIndex = +this.closest("li").getAttribute("data-index"); //Getting the number index of the li
+  // console.log(dragStartIndex); //Check
 }
 
-function dragEnter() {
-  //console.log("Event: ", "dragenter"); //check
-  this.classList.add("over");
-}
+//to manage visual feedback during the drag operation
+// function dragEnter() {
+//   console.log("Event: ", "dragenter"); //check
+//   this.classList.add("over"); //this refers to the current list item element
+// }
 
 function dragLeave() {
-  //console.log("Event: ", "dragleave"); //check
-  this.classList.remove("over");
+  console.log("Event: ", "dragleave"); //check
+  this.classList.remove("over"); //Removes CSS background color
 }
-
 function dragOver(e) {
-  //console.log("Event: ", "dragover"); //check
+  console.log("Event: ", "dragover"); //check
+  this.classList.add("over");
   e.preventDefault();
 }
 
+//When an item is dropped, the dragDrop function is called to swap the positions of the dragged item and the target item
 function dragDrop() {
   //console.log("Event: ", "drop"); //check
-  const dragEndIndex = +this.getAttribute("data-index");
+  const dragEndIndex = +this.getAttribute("data-index"); //this refers to the current list item element
   swapItems(dragStartIndex, dragEndIndex);
-
   this.classList.remove("over");
 }
 
+function addEventListeners() {
+  const draggables = document.querySelectorAll(".draggable"); //Draggable class we created inside createList()
+  const dragListItems = document.querySelectorAll(".draggable-list li");
+
+  draggables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", dragStart); //Capture the starting index
+  });
+
+  dragListItems.forEach((item) => {
+    item.addEventListener("dragover", dragOver);
+    item.addEventListener("drop", dragDrop);
+    // item.addEventListener("dragenter", dragEnter);
+    item.addEventListener("dragleave", dragLeave);
+  });
+}
+
+//swap the positions of two items in the listItems array
 function swapItems(fromIndex, toIndex) {
-  console.log("swap");
+  // console.log("swap"); //Check
   const itemOne = listItems[fromIndex].querySelector(".draggable");
   const itemTwo = listItems[toIndex].querySelector(".draggable");
-  console.log(itemOne, itemTwo);
+  //console.log(itemOne, itemTwo); //Check
 
   listItems[fromIndex].querySelector(".container-li").appendChild(itemTwo);
   listItems[toIndex].querySelector(".container-li").appendChild(itemOne);
@@ -121,7 +149,7 @@ function nextQuiz() {
   // Remove the current quiz from the array
   topics.splice(chosenQuiz, 1);
   if (topics.length > 0) {
-    console.log("next quiz");
+    // console.log("next quiz"); //Check
     // If there are more quizzes, select a random index for the next quiz
     chosenQuiz = Math.floor(Math.random() * topics.length);
     tries = 0;
@@ -139,9 +167,9 @@ function nextQuiz() {
 function checkOrder() {
   let correct = 0;
   listItems.forEach((listItem, index) => {
-    const personName = listItem.querySelector(".draggable").innerText.trim();
+    const itemName = listItem.querySelector(".draggable").innerText.trim();
 
-    if (personName !== topics[chosenQuiz].list[index]) {
+    if (itemName !== topics[chosenQuiz].list[index]) {
       listItem.classList.add("wrong");
     } else {
       listItem.classList.remove("wrong");
@@ -158,22 +186,6 @@ function checkOrder() {
       nextQuiz();
     }, 100);
   }
-}
-
-function addEventListeners() {
-  const draggables = document.querySelectorAll(".draggable");
-  const dragListItems = document.querySelectorAll(".draggable-list li");
-
-  draggables.forEach((draggable) => {
-    draggable.addEventListener("dragstart", dragStart);
-  });
-
-  dragListItems.forEach((item) => {
-    item.addEventListener("dragover", dragOver);
-    item.addEventListener("drop", dragDrop);
-    item.addEventListener("dragenter", dragEnter);
-    item.addEventListener("dragleave", dragLeave);
-  });
 }
 
 check.addEventListener("click", checkOrder);
