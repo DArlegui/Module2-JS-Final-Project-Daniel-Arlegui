36c636603e9d32a1ba7e28a36b73fd58c76d90bc
DArlegui <arleguidaniel@gmail.com>
2023-10-16 00:13:16 -0700

    reverted back to original design as intended

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-const draggable_list = document.getElementById("draggable-list"); //ul
-const check = document.getElementById("check"); //Check button
+const draggable_list = document.getElementById('draggable-list'); //ul
+const check = document.getElementById('check'); //Check button
 
 //Source: https://www.youtube.com/@DataIsBeautifulOfficial/videos
 const topics = [
   {
-    list: ["Phython", "JavaScript", "Java", "C#", "TypeScript"],
-    header: "Top 5 Most Popular Programming Languages (2022 Q3)",
+    list: ['Phython', 'JavaScript', 'Java', 'C#', 'TypeScript'],
+    header: 'Top 5 Most Popular Programming Languages (2022 Q3)',
     img: 'url("img/hacker.jpg")',
   },
   {
-    list: ["GTA 5", "League of Legends", "CS:GO", "Valorant", "COD: Warzone"],
-    header: "Top 5 Most Watched Streamed Games (2022 Q4)",
+    list: ['GTA 5', 'League of Legends', 'CS:GO', 'Valorant', 'COD: Warzone'],
+    header: 'Top 5 Most Watched Streamed Games (2022 Q4)',
     img: 'url("img/games.jpg")',
   },
   {
-    list: ["Elon Musk", "Jeff Bezos", "Bill Gates", "Mark Zuckerberg", "Warren Buffett"],
-    header: "Top 5 Richest People in the World (2023 Q1)",
+    list: [
+      'Elon Musk',
+      'Jeff Bezos',
+      'Bill Gates',
+      'Mark Zuckerberg',
+      'Warren Buffett',
+    ],
+    header: 'Top 5 Richest People in the World (2023 Q1)',
     img: 'url("img/money.jpg")',
   },
   {
-    list: ["Google", "Youtube", "Facebook", "Twitter", "Instagram"],
-    header: "Top 5 Most Popular Websites (2022 Q3)",
+    list: ['Google', 'Youtube', 'Facebook', 'Twitter', 'Instagram'],
+    header: 'Top 5 Most Popular Websites (2022 Q3)',
     img: 'url("img/web.jpg")',
   },
 ];
@@ -36,15 +42,15 @@ createList();
 //Insert List Items into DOM
 function createList() {
   //Capturing the header
-  const header = document.getElementById("title");
+  const header = document.getElementById('title');
 
   //Changing the header and body background depending on the quiz
   header.innerHTML = topics[chosenQuiz].header;
-  document.body.style.backgroundImage = topics[chosenQuiz].img;
+  // document.body.style.backgroundImage = topics[chosenQuiz].img; //Decided to stick with original design
 
   /*Clears any previous content within the HTML element with the ID "draggable-list." 
   This is done to prepare for the creation of a new list. */
-  draggable_list.innerHTML = "";
+  draggable_list.innerHTML = '';
   listItems = []; //stores the new list items
 
   // console.log(topics[chosenQuiz], topics, chosenQuiz); //Check
@@ -58,10 +64,10 @@ function createList() {
     .map((a) => a.value)
     .forEach((item, index) => {
       //iterates through the shuffled items and creates a list item (<li>) for each item.
-      const listItem = document.createElement("li"); //Creating li
+      const listItem = document.createElement('li'); //Creating li
 
       //store original index. track the order of the list items. Use for drag and drop functionality
-      listItem.setAttribute("data-index", index);
+      listItem.setAttribute('data-index', index);
 
       listItem.innerHTML = `
       <div class="container-li">
@@ -81,8 +87,8 @@ function createList() {
 
 //Capture the starting index
 function dragStart() {
-  console.log("Event: ", "dragstart"); //check
-  dragStartIndex = +this.closest("li").getAttribute("data-index"); //Getting the number index of the li
+  console.log('Event: ', 'dragstart'); //check
+  dragStartIndex = +this.closest('li').getAttribute('data-index'); //Getting the number index of the li
   // console.log(dragStartIndex); //Check
 }
 
@@ -91,62 +97,62 @@ function dragStart() {
 var counter = 0;
 
 function dragLeave() {
-  console.log("Event: ", "dragleave"); //check
+  console.log('Event: ', 'dragleave'); //check
   //we leave the deeper layer
   counter--;
   console.log(counter);
   //if we are in the outermost layer
   if (counter == 0) {
-    this.classList.remove("over"); //Removes CSS background color
+    this.classList.remove('over'); //Removes CSS background color
   }
 }
 function dragOver(e) {
-  console.log("Event: ", "dragover"); //check
+  console.log('Event: ', 'dragover'); //check
   e.preventDefault();
 }
 
 function dragEnter(e) {
-  console.log("Event: ", "dragenter"); //check
+  console.log('Event: ', 'dragenter'); //check
   console.log(counter);
   //we enter deeper layer
   counter++;
 
-  this.classList.add("over");
+  this.classList.add('over');
   e.preventDefault();
 }
 
 //When an item is dropped, the dragDrop function is called to swap the positions of the dragged item and the target item
 function dragDrop() {
   //console.log("Event: ", "drop"); //check
-  const dragEndIndex = +this.getAttribute("data-index"); //this refers to the current list item element
+  const dragEndIndex = +this.getAttribute('data-index'); //this refers to the current list item element
   swapItems(dragStartIndex, dragEndIndex);
-  this.classList.remove("over");
+  this.classList.remove('over');
   counter = 0;
 }
 
 function addEventListeners() {
-  const draggables = document.querySelectorAll(".draggable"); //Draggable class we created inside createList()
-  const dragListItems = document.querySelectorAll(".draggable-list li");
+  const draggables = document.querySelectorAll('.draggable'); //Draggable class we created inside createList()
+  const dragListItems = document.querySelectorAll('.draggable-list li');
 
   draggables.forEach((draggable) => {
-    draggable.addEventListener("dragstart", dragStart); //Capture the starting index
+    draggable.addEventListener('dragstart', dragStart); //Capture the starting index
   });
 
   dragListItems.forEach((item) => {
-    item.addEventListener("dragover", dragOver);
-    item.addEventListener("dragenter", dragEnter);
-    item.addEventListener("drop", dragDrop);
-    item.addEventListener("dragleave", dragLeave);
+    item.addEventListener('dragover', dragOver);
+    item.addEventListener('dragenter', dragEnter);
+    item.addEventListener('drop', dragDrop);
+    item.addEventListener('dragleave', dragLeave);
   });
 }
 
 //swap the positions of two items in the listItems array
 function swapItems(fromIndex, toIndex) {
-  const itemOne = listItems[fromIndex].querySelector(".draggable");
-  const itemTwo = listItems[toIndex].querySelector(".draggable");
+  const itemOne = listItems[fromIndex].querySelector('.draggable');
+  const itemTwo = listItems[toIndex].querySelector('.draggable');
 
-  listItems[fromIndex].querySelector(".container-li").appendChild(itemTwo);
-  listItems[toIndex].querySelector(".container-li").appendChild(itemOne);
+  listItems[fromIndex].querySelector('.container-li').appendChild(itemTwo);
+  listItems[toIndex].querySelector('.container-li').appendChild(itemOne);
 }
 
 function nextQuiz() {
@@ -160,7 +166,7 @@ function nextQuiz() {
     createList();
   } else {
     // If there are no more quizzes, display a message
-    alert("You have solved all the quizzes! Congrats! Now closing the tab...");
+    alert('You have solved all the quizzes! Congrats! Now closing the tab...');
     setTimeout(() => {
       window.close(); // Close the current tab (window)
     }, 100);
@@ -171,13 +177,13 @@ function nextQuiz() {
 function checkOrder() {
   let correct = 0;
   listItems.forEach((listItem, index) => {
-    const itemName = listItem.querySelector(".draggable").innerText.trim();
+    const itemName = listItem.querySelector('.draggable').innerText.trim();
 
     if (itemName !== topics[chosenQuiz].list[index]) {
-      listItem.classList.add("wrong");
+      listItem.classList.add('wrong');
     } else {
-      listItem.classList.remove("wrong");
-      listItem.classList.add("right");
+      listItem.classList.remove('wrong');
+      listItem.classList.add('right');
       correct++;
     }
   });
@@ -192,4 +198,4 @@ function checkOrder() {
   }
 }
 
-check.addEventListener("click", checkOrder);
+check.addEventListener('click', checkOrder);
